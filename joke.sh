aca1aabdcc168fb2df4453c0c6231f0fbeaa2d9b
mohammadabidhossain <abidmd.hossain@gmail.com>
2025-09-07 13:47:57 +0600

    add comment

--- joke.sh
+++ joke.sh
@@ -1,2 +1,2 @@
-
+#this line is curl-ing the content from the website in jason format then convert it to text
 curl -s https://official-joke-api.appspot.com/random_joke | jq -r '"\(.setup)\n\(.punchline)"'
